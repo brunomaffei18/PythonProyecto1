f77ab7bd4e483260e91d0c47eeba34aa6effe61a
brunomaffei18 <maffeib18@gmail.com>
2025-10-17 16:53:08 -0300

    segundo commit

--- Proyecto1/urls.py
+++ Proyecto1/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from miapp.views import saludo
 
-urlpatterns = [
+urlpatterns = [ 
     path('admin/', admin.site.urls),
+    path('saludo/', saludo), 
+
 ]
